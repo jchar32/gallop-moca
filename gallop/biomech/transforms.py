953177c6c97f7b06dde44774e575a8f098c19b4f
Jesse Charlton <57236497+jchar32@users.noreply.github.com>
2023-10-14 15:02:03 -0700

    small touch up on doc

--- gallop/biomech/transforms.py
+++ gallop/biomech/transforms.py
@@ -1,13 +1,18 @@
 import numpy as np
+import xarray as xr
 
 
-def get_fp_coordsys(raw_analog):
-    """compute and store additional force platform configurations variables (origin in fp frame, fp to gcs rotation matrix)
+def get_fp_coordsys(raw_analog: xr.DataArray):
+    """
+    Calculates the force plate coordinate system in global coordinate system.
+
+    Args:
+        raw_analog (xr.DataArray): A numpy array containing raw analog data that was generated from the io.read.c3d function. This array should be of type xarray
 
     Returns:
-        fp_origin_gcs (ndarray): origin of the FP in GCS
-        fp_cs (ndarray): rotation matrix from FP to GCS
+        tuple: A tuple containing the force plate origin in global coordinate system and the force plate coordinate system.
     """
+
     num_plates = raw_analog.attrs["FP_USED"]
     fp_origin_gcs = np.zeros((3, num_plates))
     fp_cs = np.zeros((3, 3, num_plates))
@@ -17,12 +22,8 @@ def get_fp_coordsys(raw_analog):
         fp_origin_gcs[:, plate] = np.mean(corners, axis=1)
 
         # ** force plate to GCS
-        fp_y = (corners[:, 1] - corners[:, 2]) / np.linalg.norm(
-            corners[:, 1] - corners[:, 2]
-        )
-        fp_x = (corners[:, 3] - corners[:, 2]) / np.linalg.norm(
-            corners[:, 3] - corners[:, 2]
-        )
+        fp_y = (corners[:, 1] - corners[:, 2]) / np.linalg.norm(corners[:, 1] - corners[:, 2])
+        fp_x = (corners[:, 3] - corners[:, 2]) / np.linalg.norm(corners[:, 3] - corners[:, 2])
         fp_z = np.cross(fp_x, fp_y)
         fp_cs[:, :, plate] = np.vstack((fp_x, fp_y, fp_z))
 
